c810c37f18276d4813a3d5f88cd36f4775cd53f4
CyberpOOP <vlad0021135@gmail.com>
2022-10-17 21:36:10 +0300

    working cmake and make

--- foo.c
+++ foo.c
@@ -1,6 +1,6 @@
 
 #include <stdio.h>
-#include <header.h>
+#include "foo.h"
 
 void myPrintHelloMake(void) {
 
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <header.h>
+#include "foo.h"
 
 int main() {
   myPrintHelloMake();
