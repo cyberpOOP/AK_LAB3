791edf404e97412672df238c45ee071219eef416
CyberpOOP <vlad0021135@gmail.com>
2022-10-18 20:27:07 +0300

    final changes

--- experiment/file.cpp
+++ experiment/file.cpp
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include "foo.h"
-
-void myPrintHelloMake(void) {
-
-  printf("Hello makefiles!\n");
-}
+#include<iostream>
+#include "sum.h"
+#include "sum.cpp"
 
 int main()
 {
+    int a, b;
+    std::cout<<"a=";
+    std::cin>>a;
+    std::cout<<"b=";
+    std::cin>>b;
 
+    std::cout<<"Sum is:"<<sumOfTwoNumbers(a, b)<<"\n";
 }
